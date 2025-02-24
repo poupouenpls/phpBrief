926390c0f8b6089aa08a2938609a716b96d29bc0
poupouenpls <aurel.hasteur@gmail.com>
2025-02-24 15:46:28 +0100

    feat: ajout du brief

---
+++ form.php
@@ -0,0 +1,47 @@
+<?php
+    // demarrer une session
+    session_start();
+
+    // recuperation des informations
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        // recuperation des valeurs dans les champs inputs
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email');
+        $message = filter_input(INPUT_POST, 'commataire');
+
+        // condition verification
+        if($name !== '' && $email !== '' && $message !== '' ) {
+            // stockage message dans la session
+            $_SESSION['message'] = "merci $name";
+
+            // concatenation des valeurs en une variable
+            $texte = "nom : $name\nemail : $email\nmessage : $message";
+            
+            // on definit le chemin du fichier
+            $file = __DIR__ . '/commentaire.txt';
+
+            // on ecrit le contenu dans le fichier
+            file_put_contents($file, $texte);
+
+            header("Location: form.php");
+            exit();
+        } else {
+            $_SESSION['message'] = "un probleme est survenue";
+        }
+    }
+?>
+
+<!-- affichage de la confirmation du deploiement -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <h1>Success</h1>
+    <p>Les informations sont bien enregistrer.</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+
+<body>
+    <form method="POST" action="form.php">
+        <div>
+            name : <input type="text" name="name">
+        </div>
+
+        <div>
+            email : <input type="text" name="email">
+        </div>
+
+        <div>
+            commentaire : <textarea name="commataire"></textarea>
+        </div>
+
+        <button type="submit">Envoyer</button>
+    </form>
+</body>
+</html>
